2cfac7ea2c81ca523ce01b161b4add36304d2a83
Tekofx <tekofxx@gmail.com>
2022-06-10 21:38:32 +0200

    POST games

--- backend/src/routes/games.js
+++ backend/src/routes/games.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const games = require('../games.json')
 
+
 router.get('/', async function (req, res) {
     res.header("Content-Type", 'application/json');
     return res.send(JSON.stringify(games));
@@ -15,5 +16,22 @@ router.get('/:id', async function (req, res) {
     return res.send(JSON.stringify(deck));
 });
 
+
+
+router.post('/', async function (req, res) {
+    var gameId = 1;// TODO: get a free id
+    var decks = req.body.decks;
+    var users = req.body.users;
+    var game = {
+        id: gameId,
+        decks: decks,
+        users: users,
+
+    }
+    games.push(game);
+    console.log(games);
+
+    return res.status(200).send("Game created");
+});
 
 module.exports = router;
